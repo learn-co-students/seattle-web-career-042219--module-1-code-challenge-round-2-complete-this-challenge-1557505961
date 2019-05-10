4bfdf9328cb5e634431eae16a13c8915d5c1c4d6
github username <github email address>
2019-05-10 10:40:22 -0700

    argh

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,16 @@
 class QueueItem
 
+@@all = []
+
+  def initialize(viewer, movie, rating = nil)
+    @@all << self
+    @viewer = viewer
+    @movie = movie
+    @rating = rating
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,5 +8,28 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+
+##view objects
+laura = Viewer.new("Laura")
+kev = Viewer.new("Kev")
+sarah = Viewer.new("Sarah")
+tyler = Viewer.new("Tyler")
+
+##moview objects
+goodfellas = Movie.new("Goodfellas")
+doctor_strange = Movie.new("Doctor Strange")
+beauty_beast = Movie.new("Beauty and the Beast")
+pulp_fiction = Movie.new("Pulp Fiction")
+resevoir_dogs = Movie.new("Resevoir Dogs")
+
+
+##queue objects
+q1 = QueueItem.new(laura, goodfellas, 5)
+q2 = QueueItem.new("Kev", "Goodfellas", 3)
+q3 = QueueItem.new("Tyler", "Doctor Strange", 2)
+q4 = QueueItem.new("Sarah", "Beauty and the Beast", 2)
+q5 = QueueItem.new("Laura", "Pulp Fiction", 4)
+q6 = QueueItem.new("Kev", "Resevoir Dogs", 5)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
