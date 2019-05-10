db41ef8386b792fda895695ebfbd2407ea92355c
ndnguyen07 <github email address>
2019-05-10 10:38:20 -0700

    Nailit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,16 @@ class Movie
     @@all
   end
 
+  def queue_items
+    QueueItem.all.select {|q_i| q_i.movie == self}
+  end
+
+  def viewers
+    queue_items.map {|q_i| q_i.viewer}
+  end
+
+  # def rate_movie(viewer, rating = (1..5))
+  #   QueueItem.new(viewer, self, rating)
+  # end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,5 +8,19 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+v1 = Viewer.new("u1")
+v2 = Viewer.new("u2")
+v3 = Viewer.new("u3")
+
+m1 = Movie.new("t1")
+m2 = Movie.new("t2")
+m3 = Movie.new("t3")
+
+i1 = QueueItem.new(v1, m2, 5)
+i2 = QueueItem.new(v2, m1, 2)
+i3 = QueueItem.new(v3, m3, 1)
+i4 = QueueItem.new(v2, m2, 4)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
